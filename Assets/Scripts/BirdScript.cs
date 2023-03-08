56440cd01a44650439b7b7c61786d50c9f734691
Erick Sauri <ericksauri@hey.com>
2023-03-07 21:44:46 -0500

    feat(bird): add script to flap on mouse press or spacebar press

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+    public float flapStrength;
+
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() {
+        bool shouldFlap = Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0);
+
+        if (shouldFlap) {
+            Flap();
+        }
+    }
+
+    void Flap() {
+        rigidBody.velocity = Vector2.up * flapStrength;
+    }
+}
